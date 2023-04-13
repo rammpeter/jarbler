071f74e2dae543a9f632cf65071526b50beeedd4
rammpeter <Peter@ramm-oberhermsdorf.de>
2023-04-13 14:42:49 +0200

    Copy Rails application into jar file

---
+++ lib/JarMain.java
@@ -0,0 +1,96 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.UUID;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
+import java.util.ArrayList;
+
+class JarMain {
+   static void deleteFolder(File file){
+      for (File subFile : file.listFiles()) {
+         if(subFile.isDirectory()) {
+            deleteFolder(subFile);
+         } else {
+            subFile.delete();
+         }
+      }
+      file.delete();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Start java process in jar file");
+        // create a new folder in temp directory
+        File newFolder = new File(System.getProperty("java.io.tmpdir") + File.separator + UUID.randomUUID().toString());
+        newFolder.mkdir();
+        System.out.println(newFolder.getAbsolutePath());
+        // Get the path of the jar file
+        String jarPath = JarMain.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        System.out.println("Executed jar file is: "+ jarPath);
+
+        // extract the jarFile by executing jar -xf jarFileName
+        try {
+            System.out.println("Extracting files from "+jarPath+" to "+ newFolder.getAbsolutePath());
+            ProcessBuilder pb = new ProcessBuilder("jar", "-xf", jarPath);
+            pb.directory(newFolder);
+            Process p = pb.start();
+            InputStream is = p.getInputStream();
+            int i = 0;
+            while ((i = is.read()) != -1) {
+                System.out.print((char) i);
+            }
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // get the file name of the jruby jar file in newFolder
+        File[] files = newFolder.listFiles();
+        // get the existing file from files where prefix is jruby-core* and suffix is .jar
+        File jrubyCoreFile = null;
+        File jrubyStdlibFile = null;
+        for (File file : files) {
+            System.out.println(file.getName());
+            if (file.getName().startsWith("jruby-core") && file.getName().endsWith(".jar")) {
+                jrubyCoreFile = file;
+            }
+            if (file.getName().startsWith("jruby-stdlib") && file.getName().endsWith(".jar")) {
+                jrubyStdlibFile = file;
+            }
+        }
+        System.out.println("jruby core jar file is  : "+ jrubyCoreFile.getAbsolutePath());
+        System.out.println("jruby stdlib jar file is: "+ jrubyStdlibFile.getAbsolutePath());
+
+        ArrayList<String> classpaths = new ArrayList<String>();
+        classpaths.add(jrubyCoreFile.getAbsolutePath());
+        classpaths.add(jrubyStdlibFile.getAbsolutePath());
+        String classpath = String.join(File.pathSeparator, classpaths);
+        System.out.println("Classpath is : "+ classpath);
+
+        // execute java -jar jrubyJarFile with argument config.ru
+        try {
+            ProcessBuilder pb = new ProcessBuilder("java", "-cp", classpath, "org.jruby.Main",  "bin/rails", "server");
+            pb.directory(new File(newFolder.getAbsolutePath()+File.separator+"rails_app"));
+            pb.redirectErrorStream(true);                                       // redirect error stream to output stream
+            Process p = pb.start();
+            InputStream is = p.getInputStream();
+            int i = 0;
+            while ((i = is.read()) != -1) {
+                System.out.print((char) i);
+            }
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // remove the directory newFolder recursively
+        System.out.println("Removing all content in folder "+ newFolder.getAbsolutePath());
+        //deleteFolder(newFolder);
+
+    }
+}
--- lib/jarbler.rb
+++ lib/jarbler.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'fileutils'
+require 'tmpdir'
 require_relative "jarbler/version"
 
 module Jarbler
@@ -10,8 +11,13 @@ module Jarbler
   def self.run
     puts "Jarbler release #{VERSION}"
 
+    # create a temporary directory for staging
+    staging_dir = Dir.mktmpdir
 
-    rails_root = __dir__
+    jarbler_lib_dir = __dir__
+    rails_root = Dir.pwd
+    puts "Project dir: #{rails_root}"
+    jar_name = File.basename(rails_root) + '.jar'
 
     # read the file RAILS_ROOT/.ruby-version starting from char at position 6 to the end of the line
     requested_ruby_version = File.read("#{rails_root}/.ruby-version")[6..20].strip
@@ -22,31 +28,69 @@ module Jarbler
     end
 
     # requires that default Gem location is used (no BUNDLE_PATH: "vendor/bundle" in .bundle/config)
+    jruby_jars_location = nil
     `bundle info jruby-jars`.lines.each do |line|
       if line.match(JRUBY_VERSION) && line.match(/Path:/)
-        JRUBY_JARS_LOCATION = line.split[1]
-        puts "JRUBY_JARS_LOCATION=#{JRUBY_JARS_LOCATION}"
+        jruby_jars_location = line.split[1]
+        puts "Location of jRuby jars: #{jruby_jars_location}"
       end
     end
 
     # Compile the Java files
-    puts `javac JarMain.java`
+    puts `javac -d #{staging_dir} #{jarbler_lib_dir}/JarMain.java`
 
-    # Remove all jar files
-    Dir.glob('*.jar').each { |file| File.delete(file)}
-    FileUtils.cp("#{JRUBY_JARS_LOCATION}/lib/jruby-core-#{JRUBY_VERSION}-complete.jar", '.')
-    FileUtils.cp("#{JRUBY_JARS_LOCATION}/lib/jruby-stdlib-#{JRUBY_VERSION}.jar", '.')
+    FileUtils.cp("#{jruby_jars_location}/lib/jruby-core-#{JRUBY_VERSION}-complete.jar", staging_dir)
+    FileUtils.cp("#{jruby_jars_location}/lib/jruby-stdlib-#{JRUBY_VERSION}.jar", staging_dir)
 
-    puts `jar cvfm Panorama.jar Manifest.txt JarMain.class *.jar`
+    # Copy the Rails project to the staging directory
+    FileUtils.cp_r("#{rails_root}", staging_dir)
 
-    # Show content of created jar file
-    puts "\nContent of created jar file:"
-    puts `jar tvf Panorama.jar`
+    # Use fixed name for Rails project directory
+    FileUtils.mv("#{staging_dir}/#{File.basename(rails_root)}", "#{staging_dir}/rails_app")
+
+    # Get the needed Gems
+    raise "Gemfile.lock not found in #{rails_root}" unless File.exist?("#{rails_root}/Gemfile.lock")
+
+    # Read all lines of Gemfile.lock into an array
+    gemfile_lock = File.readlines("#{rails_root}/Gemfile.lock")
+    active_line = false
+    gemfile_lock.each do |line|
+      active_line = true if line.match(/specs/)
+      active_line = false if line.match(/PLATFORMS/)
+      if active_line && !line.match(/specs/) && line[4] != ' '
+        gem_name = line.split[0]
+        gem_version = line.split[1]
+        unless gem_version.nil?                             # ignore gems without version
+          Jarbler.copy_gem_to_staging(gem_name, gem_version.gsub(/\(|\)/, ''), staging_dir)
+        end
+      end
+    end
 
 
+
+    Dir.chdir(staging_dir) do
+      # create the manifest file
+      File.open('Manifest.txt', 'w') { |file| file.write("Main-Class: JarMain\n") }
+
+      puts `jar cfm #{jar_name} Manifest.txt *`
+
+      # Show content of created jar file
+      #puts "\nContent of created jar file:"
+      #puts `jar tvf #{jar_name}`
+
+      # place the jar in project directory
+      FileUtils.cp(jar_name, rails_root)
+
+    end
+
+    # remove temporary directory staging_dir
+    FileUtils.remove_entry staging_dir
 
   end
 
   private
+  def self.copy_gem_to_staging(gem_name, gem_version, staging_dir)
+    puts "#{gem_name} #{gem_version}"
+  end
 
 end
