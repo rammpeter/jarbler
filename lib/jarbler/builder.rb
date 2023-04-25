f3e3e21334dcacc21cb1a43f6b0bc220761b7162
rammpeter <Peter@ramm-oberhermsdorf.de>
2023-04-25 14:19:00 +0200

    Fix java source location

--- lib/jarbler/builder.rb
+++ lib/jarbler/builder.rb
@@ -19,7 +19,7 @@ module Jarbler
       debug "Project dir: #{app_root}"
 
       ruby_version = copy_jruby_jars_to_staging(staging_dir) # Copy the jruby jars to the staging directory
-      exec_command "javac -nowarn -Xlint:deprecation -source 8 -target 8 -d #{staging_dir} #{app_root}/JarMain.java" # Compile the Java files
+      exec_command "javac -nowarn -Xlint:deprecation -source 8 -target 8 -d #{staging_dir} #{__dir__}/JarMain.java" # Compile the Java files
 
       # Copy the application project to the staging directory
       FileUtils.mkdir_p("#{staging_dir}/app_root")
