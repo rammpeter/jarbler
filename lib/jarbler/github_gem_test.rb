c73908ec0bf226b2ddb952e1457c40727171683c
rammpeter <Peter@ramm-oberhermsdorf.de>
2023-04-28 15:41:49 +0200

    Test branch that should never be merged Used to test a github Gem dependency in a particular branch

---
+++ lib/jarbler/github_gem_test.rb
@@ -0,0 +1,11 @@
+# GithubGemTest is a class that is used to test the Jarbler gem
+# This class should not exist in main branch to ensure valid test of branch dependency
+module Jarbler
+  class GithubGemTest
+    # Simple function which returns a keyword to indicate success
+    def check_github_gem_dependency
+      "SUCCESS"
+    end
+
+
+end
